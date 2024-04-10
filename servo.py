168f526602447c31e2e3b090328beda16da2b6ac
Gayatri <gaya3p13@gmail.com>
2024-04-10 16:41:00 +0530

    Update servo.py

--- servo.py
+++ servo.py
@@ -3,11 +3,11 @@
 import pyfirmata
 
 class servo:
-    def __init__(self,pin,board):
+    def __init__(self,pin):
         self.pin=board.get_pin(f'd:{}:s'.{pin})#board will be initialized beforehand
         
     def move_servo(self,angle):
-       self.pin.write(angle)
+        self.pin.write(angle)
         
 
 
