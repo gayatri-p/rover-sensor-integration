4f246a9142c732172a6117f482ad7891a991f901
Gayatri <gaya3p13@gmail.com>
2024-04-10 12:47:55 +0530

    Update servo.py

--- servo.py
+++ servo.py
@@ -1,26 +1,25 @@
 """ servo pyfirmata"""
-
-import pyfirmata
-from tkinter import *
-
-def move_servo(angle):
-    pin9.write(angle)
+class servo:
+    import pyfirmata
+    from tkinter import *
+    def move_servo(angle):
+        pin9.write(angle)
+        
+    def main():
+        global pin9
+        
+        board=pyfirmata.Arduino('COM5')
     
-def main():
-    global pin9
+        iter8 = pyfirmata.util.Iterator(board)
+        iter8.start()
     
-    board=pyfirmata.Arduino('COM5')
-
-    iter8 = pyfirmata.util.Iterator(board)
-    iter8.start()
-
-    pin9 = board.get_pin('d:9:s')
+        pin9 = board.get_pin('d:9:s')
+        
+        root = Tk()
+        scale = Scale(root, command = move_servo, to = 175, 
+                      orient = HORIZONTAL, length = 400, label = 'Angle')
+        scale.pack(anchor = CENTER)
     
-    root = Tk()
-    scale = Scale(root, command = move_servo, to = 175, 
-                  orient = HORIZONTAL, length = 400, label = 'Angle')
-    scale.pack(anchor = CENTER)
-
-    root.mainloop()
-
-main()
+        root.mainloop()
+    
+    main()
