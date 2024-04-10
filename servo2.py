5d8f6b6e39ede0bde111714c22ecc40b595110a8
Gayatri <gaya3p13@gmail.com>
2024-04-10 12:53:26 +0530

    Update servo2.py

--- servo2.py
+++ servo2.py
@@ -1,18 +1,18 @@
 from pyfirmata import Arduino, SERVO
 from time import sleep
-
-port=''
-pin=10
-board=Arduino(port)
-
-board.digital[pin].mode=SERVO
-
-def rotateServo(pin,angle):
-    board.digital[pin].write(angle)
-    sleep(0.015)
-
-while True:
-    for i in range(0,100):
-        rotateServo(pin,i)
-    for i in range(180,1,-1):
-        rotateServo(pin,i)
+class servo2:
+    port=''
+    pin=10
+    board=Arduino(port)
+    
+    board.digital[pin].mode=SERVO
+    
+    def rotateServo(pin,angle):
+        board.digital[pin].write(angle)
+        sleep(0.015)
+    
+    while True:
+        for i in range(0,100):
+            rotateServo(pin,i)
+        for i in range(180,1,-1):
+            rotateServo(pin,i)
